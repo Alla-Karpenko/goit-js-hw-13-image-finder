94ff6324f61bec316d25aa580266de561b64218b
alla karpenko <karpenkoallo4ka@gmail.com>
2021-03-28 02:25:54 +0200

    up

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -7,15 +7,14 @@ export default class NewsApiService {
         this.page = 1;
     }
 
-    fetchGallery() {
+    async fetchGallery() {
         console.log(this);
         const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&
         page=${this.page}&per_page=12&webformatURL&likes&views&downloads&comments&key=${API_KEY}`;
 
-       return fetch(url)
-        .then(response => response.json())
-        .then(({hits})=> hits);
-        
+       const response = await fetch(url);
+        const { hits } = await response.json();
+        return hits;
     }
 
     incrementPage () {
@@ -25,7 +24,6 @@ export default class NewsApiService {
     resetPage() {
        this.page = 1;
     }
-    
 
     get query() {
         return this.searchQuery;
